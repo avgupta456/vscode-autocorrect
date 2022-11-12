13c2573a428e54c7b8863b3c59276eb098c7fb41
Abhijit Gupta <avgupta456@gmail.com>
2022-11-12 08:47:41 -0500

    Click to correct

--- vscode/autocorrect/src/extension.ts
+++ vscode/autocorrect/src/extension.ts
@@ -6,15 +6,57 @@ export async function activate(context: vscode.ExtensionContext) {
   console.log("decorator sample is activated");
 
   let timeout: NodeJS.Timer | undefined = undefined;
+  let activeEditor = vscode.window.activeTextEditor;
 
   // create a decorator type that we use to decorate large numbers
   const contextErrorDecorationType =
     vscode.window.createTextEditorDecorationType({
-      // use a themable color. See package.json for the declaration and default values.
-      backgroundColor: { id: "myextension.contextErrorBackground" },
+      textDecoration: "underline wavy blue",
     });
 
-  let activeEditor = vscode.window.activeTextEditor;
+  /*
+  vscode.languages.registerHoverProvider("*", {
+    provideHover(document, position, token) {
+      return new vscode.Hover("Autocorrect suggests:");
+    },
+  });
+  */
+
+  const command = "autocorrect.autocorrect";
+
+  interface Args {
+    start: number;
+    end: number;
+    to: string;
+  }
+
+  const commandHandler = (args: Args) => {
+    const start = args.start;
+    const end = args.end;
+    const to = args.to;
+
+    if (!activeEditor) {
+      return;
+    }
+
+    const text = activeEditor.document.getText();
+    if (!text) {
+      return;
+    }
+
+    const range = new vscode.Range(
+      activeEditor.document.positionAt(start),
+      activeEditor.document.positionAt(end)
+    );
+
+    activeEditor.edit((editBuilder) => {
+      editBuilder.replace(range, to);
+    });
+  };
+
+  context.subscriptions.push(
+    vscode.commands.registerCommand(command, commandHandler)
+  );
 
   async function updateDecorations() {
     if (activeEditor === undefined) {
@@ -26,9 +68,19 @@ export async function activate(context: vscode.ExtensionContext) {
       (suggestion) => {
         const startPos = activeEditor!.document.positionAt(suggestion[0][1]);
         const endPos = activeEditor!.document.positionAt(suggestion[0][2]);
+        const args = [
+          { start: suggestion[0][1], end: suggestion[0][2], to: suggestion[1] },
+        ];
+        const commandUri = vscode.Uri.parse(
+          `command:${command}?${encodeURIComponent(JSON.stringify(args))}`
+        );
+        const content = new vscode.MarkdownString(
+          "[" + suggestion[1] + "](" + commandUri + ") (click to autocorrect)"
+        );
+        content.isTrusted = true;
         const decoration = {
           range: new vscode.Range(startPos, endPos),
-          hoverMessage: suggestion[1],
+          hoverMessage: content,
         };
         return decoration;
       }
