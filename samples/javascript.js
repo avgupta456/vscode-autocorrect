787e78e631963f066cd8cbd9e5523c5c9ec8c724
Abhijit Gupta <avgupta456@gmail.com>
2022-11-12 13:30:32 -0500

    Start samples

---
+++ samples/javascript.js
@@ -0,0 +1,11 @@
+// Example 1:
+// Autocorrect the word "imprt" to "import"
+
+// Template:
+// imprt { useState } from 'react';
+
+
+imprt { useState } from 'react';
+
+
+// TODO: Add more examples
---
+++ samples/python.py
@@ -0,0 +1,11 @@
+# Example 1:
+# Autocorrect the word "imprt" to "import"
+
+# Template:
+# imprt numpy as np
+
+
+import numpy as np 
+
+
+# TODO: Add more examples
