999c0f409e0bc7e4d695665678f6e68bddc835fb
Abhijit Gupta <avgupta456@gmail.com>
2022-11-12 03:25:06 -0500

    Working autocorrect
    Single line

--- backend/autocorrect.py
+++ backend/autocorrect.py
@@ -1,4 +1,6 @@
 from collections import defaultdict
+from tokenize import generate_tokens
+import re
 
 from transformers import AutoTokenizer, AutoModelForMaskedLM, pipeline
 
@@ -14,6 +16,32 @@ def get_best_output(outputs):
 
 
 def autocorrect(text, line):
-    curr_line = text.split("\n")[line]
-    print(curr_line)
-    return []
+    lines = [x + "\n" for x in text.split("\n")]
+    N = len(lines)
+
+    tokens = list(generate_tokens(lambda L=iter(lines): next(L)))
+    filtered_tokens = defaultdict(list)
+    for token in tokens:
+        filtered_tokens[token.start[0] - 1].append(token)
+    line_lengths = [filtered_tokens[i][-1].end[1] for i in range(N)]
+    cumulative_lengths = [sum(line_lengths[:i]) for i in range(N)]
+    
+    curr_tokens = filtered_tokens[line]
+    curr_token_strings = [x.string for x in curr_tokens]
+    print(" ".join(curr_token_strings).strip())
+
+    suggestions = []
+    for i in range(len(curr_tokens)):
+        prev = curr_token_strings[i]
+        curr_token_strings[i] = "<mask>"
+        string = " ".join(curr_token_strings).strip()
+        curr_token_strings[i] = prev
+        outputs = fill_mask(string)
+        best_output, best_prob = get_best_output(outputs)
+        if best_output.strip() != prev.strip() and best_prob > 0.8:
+            print("CHANGE {", prev, "} to {", best_output, "}")
+            start = cumulative_lengths[curr_tokens[i].start[0] - 1] + curr_tokens[i].start[1]
+            end = cumulative_lengths[curr_tokens[i].end[0] - 1] + curr_tokens[i].end[1]
+            suggestions.append(((prev, start, end), best_output))
+
+    return suggestions
